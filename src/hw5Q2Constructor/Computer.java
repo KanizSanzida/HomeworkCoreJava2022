21aa4d474664d67098feb79b1530f3edf9a55945
Sanzida Kaniz <92002136+SanzidaKaniz98@users.noreply.github.com>
2022-06-18 15:49:57 -0400

    Hw 5 changed

--- src/hw5Q2Constructor/Computer.java
+++ src/hw5Q2Constructor/Computer.java
@@ -11,12 +11,18 @@ public class Computer {
 	public Computer() {
 		System.out.println("This is from default constructor of Computer Class");
 	}
-	public Computer( String brand, String model, String operatingSystem, int price, char grade, boolean madeInUSA) {
-		System.out.println("My Brand: "+brand +"\nModel: "+model +"\nOperating system: "+ operatingSystem+ "\nPrice: "+price +"\nGrade: "+grade + "\nMade in USA: "+madeInUSA);
+
+	public Computer(String brand, String model, String operatingSystem, int price, char grade, boolean madeInUSA) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.operatingSystem = operatingSystem;
+		this.price = price;
+		this.grade = grade;
+		this.madeInUSA = madeInUSA;
+		System.out.println("My Brand: " + brand + "\nModel: " + model + "\nOperating system: " + operatingSystem
+				+ "\nPrice: " + price + "\nGrade: " + grade + "\nMade in USA: " + madeInUSA);
+
 	}
-
-
-
-
 
 }
